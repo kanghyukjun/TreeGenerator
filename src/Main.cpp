11e44b4e114b8c340a3a40c394aab72bba44ccfd
hyukjun kang <domohit5124@gmail.com>
2023-03-26 22:10:29 +0900

    라이브러리 추가

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
 
-int main(){
-    std::cout<<"hello opengl"<<std::endl;
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <spdlog/spdlog.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+int main(int argc, const char** argv){
+    SPDLOG_INFO("hello world");
     return 0;
 }
